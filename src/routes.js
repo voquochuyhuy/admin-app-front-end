3227a4281dcc9ed14434a0ab9317379c4002894b
VO QUOC HUY <voquochuy798@gmail.com>
2021-01-03 11:32:52 +0700

    aaa

--- src/routes.js
+++ src/routes.js
@@ -1,36 +1,34 @@
-// eslint-disable-next-line prettier/prettier
-import { AccountBalance, LaptopChromebook, Event,CalendarToday } from "@material-ui/icons";
-import Post from "./views/KLTN/post-management/post";
-import Log from "./views/KLTN/log/log";
-import User from "./views/KLTN/user-management/user";
-import Menber from "./views/KLTN/menber-management/menber";
+import Post from "./views/post-management/post";
+import Log from "./views/log/log";
+import User from "./views/user-management/user";
+import Menber from "./views/menber-management/menber";
 
 var dashRoutes = [
   {
     path: "/post-management",
     name: "Quản lí bài viết",
-    icon: Event,
+    // icon: Event,
     component: Post,
     layout: ""
   },
   {
     path: "/user-management",
     name: "Quản lí người dùng",
-    icon: AccountBalance,
+    // icon: AccountBalance,
     component: User,
     layout: ""
   },
   {
     path: "/member-management",
     name: "Quản lí thành viên",
-    icon: LaptopChromebook,
+    // icon: LaptopChromebook,
     component: Menber,
     layout: ""
   },
   {
     path: "/log",
     name: "Log",
-    icon: CalendarToday,
+    // icon: CalendarToday,
     component: Log,
     layout: ""
   }
