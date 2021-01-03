3564fc2b649de041921310ab892ea0151b8b1a61
VO QUOC HUY <voquochuy798@gmail.com>
2021-01-03 23:35:51 +0700

    aaa

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
-import Auth from "layout/auth.js";
-import Admin from "layout/admin.js";
+import Auth from "./layout/auth.js";
+import Admin from "./layout/admin.js";
 const hist = createBrowserHistory();
 
 ReactDOM.render(
